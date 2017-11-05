d6638e567e2434f0571649eef682c67ff5e4841f
Mark Bryk <brykcu@gmail.com>
2017-11-05 15:45:33 -0700

    Fix scrolling

--- js/custom.js
+++ js/custom.js
@@ -28,7 +28,7 @@ $(function() {
           if (target.length) {
               $('html,body').animate({
                   scrollTop: target.offset().top
-              }, 1000);
+              }, 500);
               return false;
           }
       }
